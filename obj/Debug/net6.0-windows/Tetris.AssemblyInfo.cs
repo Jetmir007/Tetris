0543849e974d7400fc1ed81a2c6feb609d4c0587
Jetmir007 <jeku07001@utb.vaxjo.se>
2025-04-01 14:05:47 +0200

    Tetris

--- obj/Debug/net6.0-windows/Tetris.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/Tetris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c37195d913d4a353ad5fa8e7a6c677b01d99f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9648b932da3f5604b916b2f90ebb6e21f5fa1111")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
